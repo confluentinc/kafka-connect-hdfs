6f0aa17cc30c0ff1c7f3a5139c598d52915781df
Burton Williams <burton.b.williams@gmail.com>
2016-05-26 14:47:49 -0400

    Some topic names can have dots. Make the separator only what is used for separation, and fix extraction of parts

--- src/main/java/io/confluent/connect/hdfs/HdfsSinkConnecorConstants.java
+++ src/main/java/io/confluent/connect/hdfs/HdfsSinkConnecorConstants.java
@@ -18,7 +18,7 @@ public class HdfsSinkConnecorConstants {
 
   public static final String COMMMITTED_FILENAME_SEPARATOR = "+";
 
-  public static final String COMMMITTED_FILENAME_SEPARATOR_REGEX = "[\\.|\\+]";
+  public static final String COMMMITTED_FILENAME_SEPARATOR_REGEX = "\\+";
 
   // +tmp is a invalid topic name, naming the tmp directory this way to avoid conflicts.
   public static final String TEMPFILE_DIRECTORY = "/+tmp/";
