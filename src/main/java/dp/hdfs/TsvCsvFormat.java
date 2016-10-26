317d83e524363ecc15df2d8e9b17aa4215e2e8b8
pmx <tiny1990@126.com>
2016-10-26 17:48:50 +0800

    add test

---
+++ src/main/java/dp/hdfs/TsvCsvFormat.java
@@ -0,0 +1,24 @@
+package dp.hdfs;
+
+
+import io.confluent.connect.avro.AvroData;
+import io.confluent.connect.hdfs.Format;
+import io.confluent.connect.hdfs.HdfsSinkConnectorConfig;
+import io.confluent.connect.hdfs.RecordWriterProvider;
+import io.confluent.connect.hdfs.SchemaFileReader;
+import io.confluent.connect.hdfs.hive.HiveMetaStore;
+import io.confluent.connect.hdfs.hive.HiveUtil;
+
+public class TsvCsvFormat implements Format {
+  public RecordWriterProvider getRecordWriterProvider() {
+    return new LineWriterProvider();
+  }
+
+  public SchemaFileReader getSchemaFileReader(AvroData avroData) {
+    return new LineFileReader(avroData);
+  }
+
+  public HiveUtil getHiveUtil(HdfsSinkConnectorConfig config, AvroData avroData, HiveMetaStore hiveMetaStore) {
+    return null;
+  }
+}
