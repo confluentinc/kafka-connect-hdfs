68d0ce58bee28ac10aaefda14c04674c95eefecf
Ewen Cheslack-Postava <me@ewencp.org>
2017-07-11 13:24:49 -0700

    Remove invalid offset check.
    This has been fixed since 0.10.1, so it has been long enough to allow for compatibility.

--- src/main/java/io/confluent/connect/hdfs/TopicPartitionWriter.java
+++ src/main/java/io/confluent/connect/hdfs/TopicPartitionWriter.java
@@ -80,7 +80,6 @@ public class TopicPartitionWriter {
   private AvroData avroData;
   private Set<String> appended;
   private long offset;
-  private boolean sawInvalidOffset;
   private Map<String, Long> startOffsets;
   private Map<String, Long> offsets;
   private long timeoutMs;
@@ -156,7 +155,6 @@ public class TopicPartitionWriter {
     state = State.RECOVERY_STARTED;
     failureTime = -1L;
     offset = -1L;
-    sawInvalidOffset = false;
     extension = writerProvider.getExtension();
     zeroPadOffsetFormat
         = "%0" +
@@ -508,26 +506,8 @@ public class TopicPartitionWriter {
   }
 
   private void writeRecord(SinkRecord record) throws IOException {
-    long expectedOffset = offset + recordCounter;
     if (offset == -1) {
       offset = record.kafkaOffset();
-    } else if (record.kafkaOffset() != expectedOffset) {
-      // Currently it's possible to see stale data with the wrong offset after a rebalance when you
-      // rewind, which we do since we manage our own offsets. See KAFKA-2894.
-      if (!sawInvalidOffset) {
-        log.info(
-            "Ignoring stale out-of-order record in {}-{}. Has offset {} instead of expected offset {}",
-            record.topic(), record.kafkaPartition(), record.kafkaOffset(), expectedOffset);
-      }
-      sawInvalidOffset = true;
-      return;
-    }
-
-    if (sawInvalidOffset) {
-      log.info(
-          "Recovered from stale out-of-order records in {}-{} with offset {}",
-          record.topic(), record.kafkaPartition(), expectedOffset);
-      sawInvalidOffset = false;
     }
 
     String encodedPartition = partitioner.encodePartition(record);
